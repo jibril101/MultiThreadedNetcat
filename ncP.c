5c55b47d2785622625e6eb478a18a2d28bd7dda7
Raghav Thakur <raghav.thakur.rt@gmail.com>
2019-11-15 15:56:57 -0800

    Added headers

--- ncP.c
+++ ncP.c
@@ -1,18 +1,36 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
+#include <netdb.h>
 #include "commonProto.h"
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <poll.h>
 
-int main(int argc, char **argv) {
-  
-  // This is some sample code feel free to delete it
-  // This is the main program for the poll version of nc
-  
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+// TODO: Polling, you have many connections comming in as file descriptors.
+// TODO: Poll function gives you file descriptors (sockets) and returns the index where data starts 
+// TOOD: Each file descriptor would correspond to a bit: 0 or 1 that tells 
+
+#define PORT "3490" // client will connect to this port
+#define BACKLOG 10 // the number of pending connections
+
+static void printOptions(int argc, char **argv) {
   struct commandOptions cmdOps;
   int retVal = parseOptions(argc, argv, &cmdOps);
   printf("Command parse outcome %d\n", retVal);
@@ -25,5 +43,146 @@ int main(int argc, char **argv) {
   printf("-p port = %d\n", cmdOps.source_port);
   printf("Timeout value = %d\n", cmdOps.timeout);
   printf("Host to connect to = %s\n", cmdOps.hostname);
-  printf("Port to connect to = %d\n", cmdOps.port);  
+  printf("Port to connect to = %d\n", cmdOps.port);   
+}
+
+void sigchld_handler(int s)
+{
+	(void)s; // quiet unused variable warning
+
+	// waitpid() might overwrite errno, so we save and restore it:
+	int saved_errno = errno;
+
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+
+	errno = saved_errno;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char **argv) {
+  
+  struct commandOptions cmdOps; //TODO: use cmdOps to handle different arguments passed
+  
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo *hints;
+  	struct addrinfo *servinfo; // pointer to the results
+  	struct addrinfo *p; // pointer to access each element in the struct
+	struct sockaddr_storage their_addr; // incoming connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints); // allocate memory for addrinfo struct
+	hints->ai_family = AF_INET; // forces IPv4
+	hints->ai_socktype = SOCK_STREAM;
+	hints->ai_flags = AI_PASSIVE; // use this machine's IP
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	while(1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		if (!fork()) { // this is the child process
+			close(sockfd); // child doesn't need the listener
+			if (send(new_fd, "Hello, world!", 13, 0) == -1)
+				perror("send");
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);  // parent doesn't need this
+	}
+
+  // for(pointer = server_info; pointer != NULL; pointer->ai_next) {
+    
+  // }
+
+  // // Listening mode (server)
+  // if (cmdOps.option_l && (cmdOps.option_r != 1)) {
+    
+  // }
+
+  // // Listening mode, server keeps listening for any new connections
+  // // after client disconnects from first connection
+  // if (cmdOps.option_k) {
+  //   // TODO: use while loop to have server keep waiting for connection after current connection closes (timeout)
+
+  // }
+
+  // // Listening mode, with 10 simultanours connections
+  // if (cmdOps.option_l && cmdOps.option_r) {
+
+  // }
+
+  // printOptions(argc, argv); 
+  return 0;
 }
