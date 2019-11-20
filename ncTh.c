a9fa5576582019874b63aeec470da9e8ceee1905
Raghav Thakur <raghav.thakur.rt@gmail.com>
2019-11-20 15:20:34 -0800

    Added port range check

--- ncTh.c
+++ ncTh.c
@@ -7,9 +7,23 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+#define BACKLOG 10 // queue of pending connections
 
 // function declarations
 void printOptions(struct commandOptions cmdOps, int argc, char **argv);
+
+
+
 
 typedef enum {false, true} bool;
 
@@ -19,15 +33,41 @@ struct client_fds {
   int fd;
 };
 
+void *socketThread(void *arg) {
+  int newSocket = *((int *)arg);
+}
+
 int main(int argc, char **argv) {
 
   struct commandOptions cmdOps;
   struct client_fds fd_array[10]; // array of structs
+  char portNumber[5];
+  
+  sprintf(portNumber, cmdOps.source_port); // convert int port to char*
+  puts(portNumber);
 
   printOptions(cmdOps, argc, argv);
 
   // TODO: setup socket, bind, and listen
+  int sock_fd, new_fd; // listen on sock_fd, new connection on new_fd
+
+  int socket;
+  struct addrinfo hints, *result, *pointer;
+  struct sockaddr_storage connector_addr; // connector's address info
+  socklen_t sin_size;
+  struct sigaction sa;
+  int yes = 1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
   
+
+
+
 
 
 
